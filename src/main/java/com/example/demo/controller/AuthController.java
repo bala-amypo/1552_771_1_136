931645dbf05b9a5b6cc41f7165628f7cc11ed805
bala-amypo <bala@amypo.in>
2025-12-17 08:36:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.loan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.loan.entity.User;
+import com.example.loan.service.UserService;
+
+import io.swagger.v3.oas.annotation
+
+@RestController
+@RequestMapping("/auth")
+@Tag(name="Loan Eligibility", description="Loan eligibility check")
+public class AuthController {
+    
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/{register}")
+    public User createAll(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+   @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.findByEmail(user.getEmail());
+    }
+}
