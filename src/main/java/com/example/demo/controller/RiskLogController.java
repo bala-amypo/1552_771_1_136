e28021c3d6d997d0a2f80a8cbffb9770e63e86b0
bala-amypo <bala@amypo.in>
2025-12-27 02:14:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskLogController.java
+++ src/main/java/com/example/demo/controller/RiskLogController.java
@@ -4,9 +4,11 @@ import com.example.demo.entity.RiskAssessment;
 import com.example.demo.service.RiskAssessmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api/risk-logs")
+@SecurityRequirement(name = "bearerAuth")
 public class RiskLogController {
 
     private final RiskAssessmentService service;
